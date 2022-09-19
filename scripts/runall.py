33ec75cd13f0d0537d950773301130994069e2a0
Nithi Subbaian <nithmails1@gmail.com>
2022-09-19 13:55:14 -0400

    Create runall.py

---
+++ scripts/runall.py
@@ -0,0 +1,28 @@
+Your storage is full. … You can't upload new files to Drive and may not be able to send or receive emails in Gmail.Learn more
+import runpy
+#
+runpy.run_path('clay.py')
+print("-----------------------FINISHED 1---------------------------")
+
+runpy.run_path('madison.py')
+print("-----------------------FINISHED 2---------------------------")
+
+runpy.run_path('hinds.py')
+print("-----------------------FINISHED 3---------------------------")
+
+runpy.run_path('adams.py')
+print("-----------------------FINISHED 4---------------------------")
+
+runpy.run_path('jackson.py')
+print("-----------------------FINISHED 5---------------------------")
+
+runpy.run_path('tunica.py')
+print("-----------------------FINISHED 6---------------------------")
+
+runpy.run_path('kemper.py')
+print("-----------------------FINISHED 7---------------------------")
+
+runpy.run_path('jones.py')
+print("-----------------------FINISHED 8---------------------------")
+
+print("-----------------------END---------------------------")
